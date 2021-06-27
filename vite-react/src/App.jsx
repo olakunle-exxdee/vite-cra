d1f0c22629516ee6d2059631b1ed0c1d4fd1b303
Your Name <you@example.com>
2021-06-27 02:27:33 +0100

    first-commit

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Vite</h1>
+    </div>
+  )
+}
+
+export default App
